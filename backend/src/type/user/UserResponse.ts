59856a30d82a23308ff736b874d633b374c2f2fb
junhyuk <junkling@hyper-x.ai>
2024-09-30 18:46:01 +0900

    매칭 상태 반환

--- backend/src/type/user/UserResponse.ts
+++ backend/src/type/user/UserResponse.ts
@@ -1,10 +1,11 @@
 import {User} from "../../module/users/user.entity";
 import {NotFoundException} from "@nestjs/common";
 
-export interface UserResponse {
+export interface  UserResponse {
     name: string;
     age: number;
     introduce?: string;
+    matchOpenStatus: boolean;
     boardIdsList: (number|undefined)[];
 }
 
@@ -12,8 +13,9 @@ export function toUserResponse(user: User): UserResponse{
     if(!user) throw new NotFoundException();
     const name = user.getName();
     const age = user.getAge();
+    const matchOpenStatus = user.matchOpenStatus;
     const introduce = user.getIntroduce()
     const boardIdsList= !user.boardList ? [] : user.getBoardList().map(board => {return board.getId()});
-    const result = {name, age, introduce, boardIdsList};
+    const result = {name, age, introduce, matchOpenStatus ,boardIdsList};
     return result;
 }
